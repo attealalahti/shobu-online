4bdf5845ea5dbeccd18bbad2d58b568c2c85bb98
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-10-02 15:25:11 +0300

    Add better implementation for setting passive move targets.

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -88,115 +88,51 @@ const useStore = create<State>()((set) => ({
       const newBoard = copyBoard(state.boards[boardIndex]);
       newBoard[y][x].selected = !newBoard[y][x].selected;
       if (newBoard[y][x].selected) {
-        let up1: ZeroToThree | undefined;
-        let up2: ZeroToThree | undefined;
-        let right1: ZeroToThree | undefined;
-        let right2: ZeroToThree | undefined;
-        let down1: ZeroToThree | undefined;
-        let down2: ZeroToThree | undefined;
-        let left1: ZeroToThree | undefined;
-        let left2: ZeroToThree | undefined;
-        if (y - 1 >= 0) {
-          up1 = (y - 1) as ZeroToThree;
-          if (y - 2 >= 0) {
-            up2 = (y - 2) as ZeroToThree;
-          }
-        }
-        if (x + 1 < 4) {
-          right1 = (x + 1) as ZeroToThree;
-          if (x + 2 < 4) {
-            right2 = (x + 2) as ZeroToThree;
-          }
-        }
-        if (y + 1 < 4) {
-          down1 = (y + 1) as ZeroToThree;
-          if (y + 2 < 4) {
-            down2 = (y + 2) as ZeroToThree;
-          }
-        }
-        if (x - 1 >= 0) {
-          left1 = (x - 1) as ZeroToThree;
-          if (x - 2 >= 0) {
-            left2 = (x - 2) as ZeroToThree;
-          }
-        }
-        if (up1 !== undefined) {
-          if (newBoard[up1][x].content === "empty") {
-            newBoard[up1][x].passiveMoveTarget = true;
-            if (up2 !== undefined && newBoard[up2][x].content === "empty") {
-              newBoard[up2][x].passiveMoveTarget = true;
+        const vectors: [MoveVector, MoveVector][] = [
+          [
+            { x: 0, y: 1 },
+            { x: 0, y: 2 },
+          ],
+          [
+            { x: 1, y: 1 },
+            { x: 2, y: 2 },
+          ],
+          [
+            { x: 1, y: 0 },
+            { x: 2, y: 0 },
+          ],
+          [
+            { x: 1, y: -1 },
+            { x: 2, y: -2 },
+          ],
+          [
+            { x: 0, y: -1 },
+            { x: 0, y: -2 },
+          ],
+          [
+            { x: -1, y: -1 },
+            { x: -2, y: -2 },
+          ],
+          [
+            { x: -1, y: 0 },
+            { x: -2, y: 0 },
+          ],
+          [
+            { x: -1, y: 1 },
+            { x: -2, y: 2 },
+          ],
+        ];
+        vectors.forEach((tuple) => {
+          const [short, long] = tuple;
+          const nearTile = getTile(x, y, short, newBoard);
+          if (nearTile?.content === "empty") {
+            newBoard[nearTile.y][nearTile.x].passiveMoveTarget = true;
+            const farTile = getTile(x, y, long, newBoard);
+            if (farTile?.content === "empty") {
+              newBoard[farTile.y][farTile.x].passiveMoveTarget = true;
             }
           }
-          if (
-            right1 !== undefined &&
-            newBoard[up1][right1].content === "empty"
-          ) {
-            newBoard[up1][right1].passiveMoveTarget = true;
-            if (
-              up2 !== undefined &&
-              right2 !== undefined &&
-              newBoard[up2][right2].content === "empty"
-            ) {
-              newBoard[up2][right2].passiveMoveTarget = true;
-            }
-          }
-          if (left1 !== undefined && newBoard[up1][left1].content === "empty") {
-            newBoard[up1][left1].passiveMoveTarget = true;
-            if (
-              up2 !== undefined &&
-              left2 !== undefined &&
-              newBoard[up2][left2].content === "empty"
-            ) {
-              newBoard[up2][left2].passiveMoveTarget = true;
-            }
-          }
-        }
-        if (down1 !== undefined) {
-          if (
-            right1 !== undefined &&
-            newBoard[down1][right1].content === "empty"
-          ) {
-            newBoard[down1][right1].passiveMoveTarget = true;
-            if (
-              down2 !== undefined &&
-              right2 !== undefined &&
-              newBoard[down2][right2].content === "empty"
-            ) {
-              newBoard[down2][right2].passiveMoveTarget = true;
-            }
-          }
-          if (newBoard[down1][x].content === "empty") {
-            newBoard[down1][x].passiveMoveTarget = true;
-            if (down2 !== undefined && newBoard[down2][x].content === "empty") {
-              newBoard[down2][x].passiveMoveTarget = true;
-            }
-          }
-          if (
-            left1 !== undefined &&
-            newBoard[down1][left1].content === "empty"
-          ) {
-            newBoard[down1][left1].passiveMoveTarget = true;
-            if (
-              down2 !== undefined &&
-              left2 !== undefined &&
-              newBoard[down2][left2].content === "empty"
-            ) {
-              newBoard[down2][left2].passiveMoveTarget = true;
-            }
-          }
-        }
-        if (right1 !== undefined && newBoard[y][right1].content === "empty") {
-          newBoard[y][right1].passiveMoveTarget = true;
-          if (right2 !== undefined && newBoard[y][right2].content === "empty") {
-            newBoard[y][right2].passiveMoveTarget = true;
-          }
-        }
-        if (left1 !== undefined && newBoard[y][left1].content === "empty") {
-          newBoard[y][left1].passiveMoveTarget = true;
-          if (left2 !== undefined && newBoard[y][left2].content === "empty") {
-            newBoard[y][left2].passiveMoveTarget = true;
-          }
-        }
+        });
       } else {
         clearMoveTargets(newBoard);
       }
