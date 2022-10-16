04f1094665f1df395c5bb641ca35ed7652eaad72
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-10-16 20:57:56 +0300

    Add Modal component.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,19 @@
+import { createPortal } from "react-dom";
+type ModalProps = {
+  children: React.ReactNode;
+  open: boolean;
+};
+const Modal = ({ children, open }: ModalProps) => {
+  if (!open) return null;
+  return createPortal(
+    <>
+      <div className="fixed top-0 bottom-0 left-0 right-0 bg-black opacity-70" />
+      <div className="fixed top-2/4 left-2/4 h-screen w-screen -translate-y-1/2 -translate-x-1/2 p-5 py-10">
+        {children}
+      </div>
+    </>,
+    document.getElementById("portal") as HTMLElement
+  );
+};
+
+export default Modal;
